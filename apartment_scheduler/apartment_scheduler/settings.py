f01cecee0455b21f2844743f0318a25f61897385
tomeian <maria.tomeyan70@gmail.com>
2024-10-04 20:29:15 +0200

    migration files for the database as well as updated settings with required changed for database and app

--- apartment_scheduler/apartment_scheduler/settings.py
+++ apartment_scheduler/apartment_scheduler/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
+    "rest_framework",
+    "scheduling",
 ]
 
 MIDDLEWARE = [
@@ -74,9 +77,13 @@ WSGI_APPLICATION = "apartment_scheduler.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'apartment_scheduler_db',
+        'USER': 'scheduler_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
 
